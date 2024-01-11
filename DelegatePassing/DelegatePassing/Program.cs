06f1436b047a74f4a0651e85643a166fb4c46a36
WillChamness <105455707+WillChamness@users.noreply.github.com>
2024-01-11 17:33:01 -0500

    add new feature
    message box shows up at application start

--- DelegatePassing/DelegatePassing/Program.cs
+++ DelegatePassing/DelegatePassing/Program.cs
@@ -14,6 +14,7 @@ namespace DelegatePassing
         [STAThread]
         static void Main()
         {
+            MessageBox.Show("Hello World");
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
